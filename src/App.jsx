b073047dfa9c60b017e6d3058e1c60084b0ea3ab
stephanobenny03 <stephanobenny03gmail.com>
2026-07-11 11:54:19 +0530

    Search Registration

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import AddRegistration from './components/AddRegistration'
+import SearchRegistration from './components/SearchRegistration'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
 <>
 <AddRegistration/>
+<SearchRegistration/>
 </>
   )
 }
